9461707a0ce51c791e716d5b6f438939ca9ac900
vasloc202 <nvloc2510z@gmail.com>
2022-02-21 00:05:36 +0700

    Feat : Fail increase product from cart

--- src/page/cart.js
+++ src/page/cart.js
@@ -1,6 +1,5 @@
-import toastr from "toastr";
 import { reRender } from "../utils";
-import { decreaseQuantity, increaseQuantity, removeItemInCart } from "../utils/cart";
+import { increaseQuantity } from "../utils/cart";
 import "toastr/build/toastr.min.css";
 
 const CartPage = {
@@ -9,23 +8,25 @@ const CartPage = {
         if (localStorage.getItem("cart")) {
             cart = JSON.parse(localStorage.getItem("cart"));
         }
-        return `
+        return /* html */`
         <table>
             <thead>
             <tr>
                 <th>Tên sản phẩm</th>
                 <th>Giá sản phẩm</th>
                 <th>Số lượng</th>
+                <th>Ảnh</th>
                 <th></th>
                 <th></th>
             </tr>
             </thead>
             <tbody>
-            ${cart.map((item) => `
+            ${cart.map((item) => /* html */`
                 <tr>
                     <td>${item.name}</td>
                     <td>${item.price}</td>
                     <td><input type="number" value="${item.quantity}" /></td>
+                    <td><img src="${item.img}" width="100px"></td>
                     <td>
                         <button data-id="${item.id}" class="btn btn-increase">Tăng</button>
                         <button data-id="${item.id}" class="btn btn-decrease">Giảm</button>
@@ -47,16 +48,7 @@ const CartPage = {
             btn.addEventListener("click", () => {
                 if (btn.classList.contains("btn-increase")) {
                     increaseQuantity(id, () => {
-                        reRender(CartPage, "#product");
-                    });
-                } else if (btn.classList.contains("btn-decrease")) {
-                    decreaseQuantity(id, () => {
-                        reRender(CartPage, "#product");
-                    });
-                } else {
-                    removeItemInCart(id, () => {
-                        reRender(CartPage, "#product");
-                        toastr.success("Xóa sản phẩm thành công");
+                        reRender(CartPage, "#products");
                     });
                 }
             });
--- src/utils/cart.js
+++ src/utils/cart.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-plusplus */
 let cart = [];
 if (localStorage.getItem("cart")) {
     cart = JSON.parse(localStorage.getItem("cart"));
@@ -6,7 +5,6 @@ if (localStorage.getItem("cart")) {
 
 export const addTocart = (newProduct, next) => {
     const existProduct = cart.find((item) => item.id === newProduct.id);
-
     if (!existProduct) {
         cart.push(newProduct);
     } else {
@@ -16,27 +14,8 @@ export const addTocart = (newProduct, next) => {
     next();
 };
 export const increaseQuantity = (id, next) => {
+    // eslint-disable-next-line no-plusplus
     cart.find((item) => item.id === id).quantity++;
-    localStorage.setItem("cart", JSON.stringify(cart));
-    next();
-};
-export const decreaseQuantity = (id, next) => {
-    const currentProduct = cart.find((item) => item.id === id);
-    currentProduct.quantity--;
-    if (currentProduct.quantity < 1) {
-        const confirm = window.confirm("Bạn có chắc chắn muốn xóa không?");
-        if (confirm) {
-            cart = cart.filter((item) => item.id !== id);
-        }
-    }
-    localStorage.setItem("cart", JSON.stringify(cart));
-    next();
-};
-export const removeItemInCart = (id, next) => {
-    const confirm = window.confirm("Bạn có chắc chắn muốn xóa không?");
-    if (confirm) {
-        cart = cart.filter((item) => item.id !== id);
-    }
     localStorage.setItem("cart", JSON.stringify(cart));
     next();
 };
